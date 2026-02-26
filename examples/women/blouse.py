3f1627a831a720c57366b9160ebc70a011e80c99
Sarah Leweke <sarah@leweke.info>
2026-02-26 15:06:06 +0100

    Clean up blouse

--- examples/women/blouse.py
+++ examples/women/blouse.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 """Blouse without arms"""
 
-import math
-import numpy as np
 
 from sewpat.geometry import (
     Point,
     Segment,
-    Line,
-    Ray,
-    Circle,
     intersect,
     segment_to_intersection,
     CM,
-    MM,
 )
 from sewpat.measurments import make_blouse_measurements
+from sewpat.pages import DinA0
 from sewpat.part import PatternPart
 from sewpat.render import export_pattern_part_svg_mm
 from sewpat.measurments import Allowance, BlouseMeasurements, ModelConfig
@@ -64,69 +59,69 @@ def make_blouse(meas: BlouseMeasurements, model: ModelConfig) -> PatternPart:
 
     ## STEP 2.1
     # Anchor: top left
-    pt1 = Point(0, 0, "p1")
+    pt1 = Point(5 * CM, 5 * CM, "p1")
 
     ## STEP 2.2
     pt2 = pt1.translate(0, model.MoL)
     elems.append(Segment(pt1, pt2, "Modelllänge"))
-    segSaumlinie = Segment(pt2, pt2.translate(60 * CM, 0), "Saumlinie")
-    elems.append(segSaumlinie)
-    segSchulterlinie = Segment(pt1, pt1.translate(25 * CM, 0), "Schulterlinie")
-    elems.append(segSchulterlinie)
+    seg_saumlinie = Segment(pt2, pt2.translate(60 * CM, 0), "Saumlinie")
+    elems.append(seg_saumlinie)
+    seg_schulterlinie = Segment(pt1, pt1.translate(25 * CM, 0), "Schulterlinie")
+    elems.append(seg_schulterlinie)
 
     ## STEP 2.3
     pt3 = pt1.translate(0, meas.AlT)
-    segBrustlinie = Segment(pt3, pt3.translate(60 * CM, 0), "Brustlinie")
-    elems.append(segBrustlinie)
+    seg_brustlinie = Segment(pt3, pt3.translate(60 * CM, 0), "Brustlinie")
+    elems.append(seg_brustlinie)
 
     ## STEP 2.4
     pt4 = pt1.translate(0, meas.RüL)
-    segTaillenlinie = Segment(pt4, pt4.translate(60 * CM, 0), "Taillenlinie")
-    elems.append(segTaillenlinie)
+    seg_taillenlinie = Segment(pt4, pt4.translate(60 * CM, 0), "Taillenlinie")
+    elems.append(seg_taillenlinie)
 
     ## STEP 2.5
     pt5 = pt4.translate(0, meas.HüT)
-    segHüftlinie = Segment(pt5, pt5.translate(60 * CM, 0), "Hüftlinie")
-    elems.append(segHüftlinie)
+    seg_hueftlinie = Segment(pt5, pt5.translate(60 * CM, 0), "Hüftlinie")
+    elems.append(seg_hueftlinie)
 
     ## STEP 2.6
     pt6 = pt4.translate(model.BeckenAdjustment, 0)
     pt9 = pt2.translate(model.BeckenAdjustment, 0)
-    pt7 = intersect(Segment(pt1, pt6), segBrustlinie)[0]
+    pt7 = intersect(Segment(pt1, pt6), seg_brustlinie)[0]
     elems.append(Segment(pt1, pt6))
     elems.append(Segment(pt6, pt9))
 
     ## STEP 3.1
     pt10 = pt7.translate(meas.RüB, 0)
     # Invertiere die Richtung der Normalen, damit der Strahl korrekt auf den Schnittpunkt zeigt
-    _, s = segment_to_intersection(pt10, -segBrustlinie.unit_normal, segSchulterlinie)
+    _, s = segment_to_intersection(pt10, -seg_brustlinie.unit_normal, seg_schulterlinie)
     s.name = "hintere Armlinie"
     elems.append(s)
-    _, s = segment_to_intersection(pt10, segBrustlinie.unit_normal, segHüftlinie)
+    _, s = segment_to_intersection(pt10, seg_brustlinie.unit_normal, seg_hueftlinie)
     elems.append(s)
 
     ## STEP 3.2
     pt11 = pt10.translate(meas.ArD * 2 / 3, 0)
-    _, s = segment_to_intersection(pt11, segSaumlinie.unit_normal, segHüftlinie)
+    _, s = segment_to_intersection(pt11, seg_saumlinie.unit_normal, seg_hueftlinie)
     s.name = "Seitenlinie RT"
     elems.append(s)
 
     ## STEP 3.3
     pt12 = pt11.translate(10 * CM, 0)
-    _, s = segment_to_intersection(pt12, segBrustlinie.unit_normal, segHüftlinie)
+    _, s = segment_to_intersection(pt12, seg_brustlinie.unit_normal, seg_hueftlinie)
     s.name = "Seitenline VT"
     elems.append(s)
 
     ## STEP 3.4
     pt13 = pt12.translate(meas.ArD / 3, 0)
     elems.append(Segment(pt13, pt13.translate(0, -25 * CM)))
-    _, s = segment_to_intersection(pt13, segBrustlinie.unit_normal, segTaillenlinie)
+    _, s = segment_to_intersection(pt13, seg_brustlinie.unit_normal, seg_taillenlinie)
     s.name = "vordere Armlinie"
     elems.append(s)
 
     ## STEP 3.5
     pt14 = pt13.translate(meas.BrB, 0)
-    _, s = segment_to_intersection(pt14, segBrustlinie.unit_normal, segHüftlinie)
+    _, s = segment_to_intersection(pt14, seg_brustlinie.unit_normal, seg_hueftlinie)
     s.name = "vordere Mitte (vM)"
     elems.append(s)
     elems.append(Segment(pt14, pt14.translate(0, -30 * CM)))
@@ -144,4 +139,6 @@ if __name__ == "__main__":
     measurements = make_blouse_measurements(person, allowance, balance)
     model_config = make_model_config()
     part = make_blouse(measurements, model_config)
-    export_pattern_part_svg_mm(part, width_mm=1300, height_mm=1300, filename="women/blouse.svg")
+    export_pattern_part_svg_mm(
+        part, width_mm=DinA0.width, height_mm=DinA0.height, filename="women/blouse.svg"
+    )
